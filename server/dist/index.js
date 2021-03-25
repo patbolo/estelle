0799cb48fd231020114686101a2f7c872360a340
Jean-Fabien Barrois <jfbarrois@gmail.com>
2021-03-25 15:44:35 +1300

    MINOR Removed /server/dist from git

--- server/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.app = void 0;
-var server_1 = require("./server");
-var app = new server_1.AppServer().getApp();
-exports.app = app;
