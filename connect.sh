fcf81c8347db7649004da59b6304faecbdb29b7b
Jean-Fabien Barrois <jfbarrois@gmail.com>
2021-03-25 15:44:50 +1300

    MINOR Shell scripts

---
+++ connect.sh
@@ -0,0 +1,1 @@
+ssh -i "/home/jean/code/estelle/jean.pem" ec2-user@ec2-3-104-76-237.ap-southeast-2.compute.amazonaws.com
---
+++ sync.sh
@@ -0,0 +1,1 @@
+rsync -avz . -e "ssh -i /home/jean/code/estelle/jean.pem" ec2-user@ec2-3-104-76-237.ap-southeast-2.compute.amazonaws.com:~/estelle
